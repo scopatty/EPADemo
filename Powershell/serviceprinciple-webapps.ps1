6099fd71497758292bcef96d0bb89486aa059bc7
Scott Patterson <scott.patterson@live.co.uk>
2025-06-25 13:39:49 +0100

    Changes

--- Powershell/serviceprinciple-webapps.ps1
+++ Powershell/serviceprinciple-webapps.ps1
@@ -1,7 +1,5 @@
-
 $servicePrincipalName = "github-deployer"
-
-$resourceGroup= "rg-uks-webapps"
+$resourceGroup = "rg-uks-webapps"
 
 # --- 1. Create the Service Principal ---
 $spOutput = az ad sp create-for-rbac `
@@ -16,14 +14,12 @@ $appId = $spOutput.appId
 $password = $spOutput.password
 $tenantId = $spOutput.tenant
 
-
-# --- 2. Assign Contributor Role to Resource Group 1 ---
+# --- 2. Assign Contributor Role to Resource Group ---
 az role assignment create `
     --assignee $appId `
     --role "Contributor" `
     --resource-group $resourceGroup
-    --scope $id
 
-Write-host: "AppID: $appid, Secret: $password, Tenant ID: $tenantId"
+Write-Host "AppID: $appId, Secret: $password, Tenant ID: $tenantId"
 # Important: Store the appId, tenantId, and password (client secret) securely.
 # You will need these credentials for applications or services to authenticate using this Service Principal.
