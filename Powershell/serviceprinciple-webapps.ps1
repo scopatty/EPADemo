8b0f40321b8223dba3ce17e80a43c9ef729fdcae
Scott Patterson <scott.patterson@live.co.uk>
2025-06-25 13:37:16 +0100

    changes to deployment of SP's

---
+++ Powershell/serviceprinciple-webapps.ps1
@@ -0,0 +1,29 @@
+
+$servicePrincipalName = "github-deployer"
+
+$resourceGroup= "rg-uks-webapps"
+
+# --- 1. Create the Service Principal ---
+$spOutput = az ad sp create-for-rbac `
+    --name $servicePrincipalName `
+    --role "Contributor" `
+    --scopes "/subscriptions/$(az account show --query id -o tsv)/resourceGroups/$resourceGroup" `
+    --query "{appId: appId, password: password, tenant: tenant}" `
+    | ConvertFrom-Json
+
+# Extract the appId and password (client secret)
+$appId = $spOutput.appId
+$password = $spOutput.password
+$tenantId = $spOutput.tenant
+
+
+# --- 2. Assign Contributor Role to Resource Group 1 ---
+az role assignment create `
+    --assignee $appId `
+    --role "Contributor" `
+    --resource-group $resourceGroup
+    --scope $id
+
+Write-host: "AppID: $appid, Secret: $password, Tenant ID: $tenantId"
+# Important: Store the appId, tenantId, and password (client secret) securely.
+# You will need these credentials for applications or services to authenticate using this Service Principal.
